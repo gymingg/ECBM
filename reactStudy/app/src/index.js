62e302db2ab15d394db518e712c9377a139cf61f
mingwudi <1628@qq.com>
2021-01-09 15:47:07 +0800

    最新一次

--- reactStudy/app/src/index.js
+++ reactStudy/app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 //import 'antd/dist/antd.css'
 
 import Container from './App'
--- reactStudy/app/src/pages/login/login.jsx
+++ reactStudy/app/src/pages/login/login.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from './login.module.less'
 
 //登入的组件
 export default class Login extends React.Component{
     render(){
         return (
-            <div>Login组件</div>
+            <div className={styles.login}>Login组件</div>
         )
     }
 }
